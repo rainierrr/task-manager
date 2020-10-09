e6d13e3975b99070d3e10b1d7c65dfacf3cf892c
miyakawamizuki <b1901780@planet.kanazawa-it.ac.jp>
2020-10-09 16:01:49 +0900

    Mizuki_python

--- conflict/Mizuki Miyakawa.py
+++ conflict/Mizuki Miyakawa.py
@@ -1,6 +1,13 @@
+def text(x):
+    print(x[0]);
+    print(x[1]);
+    print(x[2]);
+    
 def main(n):
     for i in range(n):
         print("hello world")
 
 if __name__ == "__main__":
     main(3)
+    text('KIT')
+    
