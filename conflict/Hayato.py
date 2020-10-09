5d8225099043ec5a2b3284c324f33f0cbee0074b
Hayato <b1916582@kanazawa-it.ac.jp>
2020-10-09 15:44:26 +0900

    Hayato_python

--- conflict/Hayato.py
+++ conflict/Hayato.py
@@ -1,6 +1,11 @@
+def mein(n):
+    for i in range(n):
+        print("Saito Hayato")
+        
 def main(n):
     for i in range(n):
         print("hello world")
 
 if __name__ == "__main__":
     main(3)
+    mein(5)
